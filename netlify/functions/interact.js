be10f7722f374583f6a039023a95c1afbecac316
Courtney <courtney@3strand.dev>
2025-08-06 14:34:26 -0400

    Include prayer request in submission handler

--- netlify/functions/interact.js
+++ netlify/functions/interact.js
@@ -6,16 +6,18 @@ exports.handler = async (event) => {
 
   if (payload.type === 'view_submission' && payload.view.callback_id === 'kindness_modal') {
     const values = payload.view.state.values;
-    const description = values.description_block.description.value;
-    const type = values.type_block.kindness_type.selected_option.value;
-    const anon = values.anon_block.anon_choice.selected_option.value;
-    const username = payload.user.name;
+	const description = values.description_block.description.value;
+	const prayer = values.prayer_block?.prayer?.value || '';
+	const anon = values.anon_block.anon_choice.selected_option.value;
+	const username = payload.user.name;
 
-    // Determine the message text
-    const text = anon === 'yes'
-      ? `A 3 Strand teammate shared: _"${description}"_ (${type})`
-      : `${username} shared: _"${description}"_ (${type})`;
+	let text = anon === 'yes'
+	  ? `A 3 Strand teammate shared: _"${description}"_`
+	  : `${username} shared: _"${description}"_`;
 
+	if (prayer) {
+	  text += `\n🙏 Prayer request: _"${prayer}"_`;
+	}
     // Post to Slack channel
     await axios.post('https://slack.com/api/chat.postMessage', {
       channel: process.env.CHANNEL_ID,
